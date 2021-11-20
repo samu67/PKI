4d651d5667ae10a6b73e914445e739d2ddef6dc8
sam <sbedassa67@gmail.com>
2021-11-20 13:54:31 +0100

    latest WEb

--- WebAapp/app.py
+++ WebAapp/app.py
@@ -1,47 +1,13 @@
 from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
+
 import requests
 
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
 
-url = "url"
 
-class Credentials(db.Model):
-    userID = db.Column(db.String, primary_key=True)
-    lastName = db.Column(db.String(20), nullable=True)
-    firstName = db.Column(db.String(20), nullable=True)
-    email = db.Column(db.String(20), nullable=True)
-    pwd = db.Column(db.String(20), nullable=True)
-
-    def __repr__(self):
-        return '<New Credential %r> %self.userID'
-
-class userID_passwdHash(db.Model):
-    userID = db.Column(db.String, primary_key=True)
-    passwordHash = db.Column(db.String(20), nullable=False)
-
-    def __repr__(self):
-        return '<New Credential %r> %self.userID'
-
-class userID_certs(db.Model):
-    serialN = db.Column(db.Integer, nullable=False, primary_key=True)
-    userID = db.Column(db.String, nullable=False)
-    cert = db.Column(db.String(100), nullable=False)
-    revoked = db.Column(db.Boolean, nullable=False)
-
-    def __repr__(self):
-        return '<New Credential %r> %self.userID'
-
-class stats(db.Model):
-    nIssuedCerts = db.Column(db.Integer, nullable=False,primary_key=True)
-    nRevokedCerts = db.Column(db.Integer, nullable=False)
-    currentSN = db.Column(db.Integer, nullable=False)
-    def __repr__(self):
-        return '<New Credential %r> %self.nIssuedCerts'
+url = "http://127.0.0.1:8000"
 
 
 @app.route('/', methods = ['POST', 'GET'])
@@ -49,10 +15,10 @@ def login():
     if request.method == 'POST':
         #check for credentials first
         try:
-            username = request.form['username']
+            uid = request.form['username']
             password = request.form['password']
 
-            response = requests.post(url+"/login", json={'uid':username, 'pwd':password}).json()
+            response = requests.post(url+"/login", json={'uid': uid, 'pwd': password}).json()
 
             #userID==Username, username not changable primary key
 
@@ -61,7 +27,7 @@ def login():
 
             if response['valid']:
                 #do some session management
-                return redirect('/user/'+username)
+                return redirect('/user/'+uid)
             else:
                 return render_template('login.html')
             #check if user is admin, if so render admin
@@ -70,16 +36,16 @@ def login():
     elif request.method == 'GET':
         return render_template('login.html')
 
-@app.route('/user/<string:username>', methods=['GET'])
-def user(username):
+@app.route('/user/<string:uid>', methods=['GET'])
+def user(uid):
     #i should get everyting form db with the username
-    (credentials,certs) = getUserInfo(username)
-    return render_template('user.html', user=credentials,certs=certs)
+    (credentials,certs) = getUserInfo(uid)
+    return render_template('user.html', credentials=credentials, certs=certs)
 
-def getUserInfo(username):
+def getUserInfo(uid):
     try:
-        credentials = requests.get(url+"/credentials", json={'uid':username}).json()
-        certs = requests.get(url+"/certificates", json={'uid':username}).json()
+        credentials = requests.get(url +"/credentials", json={'uid':uid}).json()
+        certs = requests.get(url +"/certificates", json={'uid':uid}).json()
         return (credentials,certs)
     except:
         return "faild to connect to db"
@@ -87,58 +53,59 @@ def getUserInfo(username):
 
 
 
-@app.route('/updateLastName/<string:username>', methods=['POST'])
-def updateLastName(username):
+@app.route('/updateLastName/<string:uid>', methods=['POST'])
+def updateLastName(uid):
     newLastName = request.form['newLastName']
     try:
-        response = requests.post(url+"/credentials", json={'uid':username, 'lastname':newLastName, 'pwd':"",'firsrname':"",'email':""}).json()
-        if response == "Success":
-            return redirect('/user/username')
+
+        response = requests.post(url +"/credentials", json={"uid":uid, "lastname":newLastName, "pwd": "", "firstname": "", "email": ""}).json()
+        if response["Success"]:
+            return redirect('/user/'+uid)
         else:
            return "update uncessessfull"
     except:
          return "faild to connect to server"
 
 
-@app.route('/updateFirstName/<string:username>', methods=['POST'])
-def updateFirstName(username):
+@app.route('/updateFirstName/<string:uid>', methods=['POST'])
+def updateFirstName(uid):
     newFirstName = request.form['newFirstName']
     try:
-        response = requests.post(url+"/credentials", json={'uid':username, 'lastname':"", 'pwd':"",'firsrname':newFirstName,'email':""}).json()
+        response = requests.post(url +"/credentials", json={'uid': uid, 'lastname': "", 'pwd': "", 'firstname':newFirstName, 'email': ""}).json()
         if response == "Success":
-            return redirect('/user/username')
+            return redirect('/user/'+uid)
         else:
            return "update uncessessfull"
     except:
          return "faild to connect to server"
 
 
-@app.route('/updateEmail/<string:username>', methods=['POST'])
-def updateEmail(username):
+@app.route('/updateEmail/<string:uid>', methods=['POST'])
+def updateEmail(uid):
     newEmail = request.form['newEmail']
     try:
-        response = requests.post(url+"/credentials", json={'uid':username, 'lastname':"", 'pwd':"",'firsrname':"",'email':newEmail}).json()
+        response = requests.post(url +"/credentials", json={'uid': uid, 'lastname': "", 'pwd': "", 'firstname': "", 'email':newEmail}).json()
         if response == "Success":
-            return redirect('/user/username')
+            return redirect('/user/'+uid)
         else:
            return "update uncessessfull"
     except:
          return "faild to connect to server"
 
-    return redirect('/user/username')
-@app.route('/updatePassword/<string:username>', methods=['POST'])
-def updatePassword(username):
-    (CRL, certs,user) = getUserInfo(username)
+    return redirect('/user/uid')
+@app.route('/updatePassword/<string:uid>', methods=['POST'])
+def updatePassword(uid):
+
     currentPassword = request.form['currentPassword']
     newPassword0 = request.form['newPassword0']
     newPassword1 = request.form['newPassword1']
     try:
-        response = requests.post(url+"/login", json={'uid':username, 'pwd':currentPassword}).json()
+        response = requests.post(url +"/login", json={'uid':uid, 'pwd':currentPassword}).json()
         if response['valid']:
             if(newPassword1==newPassword0):
-                response = requests.post(url+"/credentials", json={'uid':username, 'lastname':"", 'pwd':newPassword1,'firsrname':"",'email':""}).json()
+                response = requests.post(url +"/credentials", json={'uid':uid, 'lastname': "", 'pwd':newPassword1, 'firstname': "", 'email': ""}).json()
                 if response == "Success":
-                    return redirect('/user/username')
+                    return redirect('/user/'+uid)
                 else:
                    return "update uncessessfull"
             else:
@@ -152,40 +119,40 @@ def updatePassword(username):
 
 
 
-@app.route('/requestNewCert/<string:username>', methods=['POST'])
-def requestNewCert(username):
+@app.route('/requestNewCert/<string:uid>', methods=['POST'])
+def requestNewCert(uid):
     try:
-        response = requests.post(url+"/certificates", json={'uid':username}).json()
+        response = requests.post(url +"/certificates", json={'uid':uid}).json()
         if response.cert != None:
-            return redirect('/user/username')
+            return redirect('/user/'+uid)
         else:
             return "faild to issue new cert"
     except:
         return "faild to connect to server"
 
 
-@app.route('/downloadCrl/<string:username>', methods=['POST'])
-def downloadCrl(username):
+@app.route('/downloadCrl/<string:uid>', methods=['POST'])
+def downloadCrl(uid):
     try:
         crl = requests.get(url+"/revoked").json()
-        return redirect('/user/username')
+        return redirect('/user/'+uid)
         #somehow start downloading crl on user page
     except:
         return "faild to connect to db"
 
 
 
-@app.route('/logout/<string:username>', methods=['POST'])
-def logout(username):
+@app.route('/logout/<string:uid>', methods=['POST'])
+def logout(uid):
     #do session management, remove current user from curret logged llist, remove session id
     return redirect('/')
 
-@app.route('/revokeCert/<string:username>$<string:serialN>', methods=['POST'])
-def revokeCert(username,serialN):
+@app.route('/revokeCert/<string:uid>$<string:serialN>', methods=['POST'])
+def revokeCert(uid, serialN):
     try:
-        response = requests.post(url+"/revoked", json={'uid':username, "serialnumber":serialN}).json()
+        response = requests.post(url +"/revoked", json={'uid':uid, "serialnumber":serialN}).json()
         if(response["Success"==1]):
-            return redirect('/user/username')
+            return redirect('/user/'+uid)
         else:
             return "revokation unsuccessfull"
     except:
