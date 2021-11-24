7f3fc3c2d316c300477d8d8b073274cb06121139
ccharollais <ccharollais@ethz.ch>
2021-11-24 12:00:19 +0100

    refactored genkeys.py + generated keys

--- ca_server/genkeys.py
+++ ca_server/genkeys.py
@@ -3,7 +3,7 @@ from cryptography.hazmat.primitives import serialization
 from cryptography import x509
 from cryptography.x509.oid import NameOID
 from cryptography.hazmat.primitives import hashes
-
+from secrets import token_urlsafe
 import datetime
 
 # Define name attribute of CA
@@ -13,7 +13,7 @@ ca_name = x509.Name([
 
 # Generate signing key of CA
 CAkey = ec.generate_private_key(ec.SECP384R1())
-with open("CAkey.pem", "wb") as f:
+with open("out/CAkey.pem", "wb") as f:
     f.write(CAkey.private_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.TraditionalOpenSSL,
@@ -22,77 +22,53 @@ with open("CAkey.pem", "wb") as f:
     f.close()
 
 
-# The following block is essentially the default way to generate a key-pair and certificate for
-# any kind of server and write it all to a file.
-# If you need more certificates, just copy this and replace the variable names (where necessary),
-# the name of the pem files and the name of the subject.
-# Generate private and public key of the server
-ServerKey = ec.generate_private_key(ec.SECP384R1())
-ServerPubKey = ServerKey.public_key()
-with open("ServerKey.pem", "wb") as f:
-    f.write(ServerKey.private_bytes(
+def gen_key_cert_signed(ca_name, CAkey, Server_name, password=None):
+
+    if password == None:
+        password = token_urlsafe(32)
+    
+    # Generate private and public key of the database
+    Key = ec.generate_private_key(ec.SECP384R1())
+    PubKey = Key.public_key()
+    with open(f"out/{Server_name}-Key.pem", "wb") as f:
+        f.write(Key.private_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.TraditionalOpenSSL,
-        encryption_algorithm=serialization.BestAvailableEncryption(b"password"),
+        encryption_algorithm=serialization.BestAvailableEncryption(password.encode()),
     ))
-    f.close()
+        f.close()
 
-# Generate certificate for the Server
-subject = x509.Name([
-    x509.NameAttribute(NameOID.COMMON_NAME, u"Server"),
-])
-server_cert = x509.CertificateBuilder().subject_name(
-    subject
-).issuer_name(
-    ca_name
-).public_key(
-    ServerPubKey
-).serial_number(
-    x509.random_serial_number()
-).not_valid_before(
-    datetime.datetime.utcnow()
-).not_valid_after(
-    datetime.datetime.utcnow().replace(year=datetime.datetime.utcnow().year + 1)
-).sign(CAkey, hashes.SHA256())
+    with open(f"out/{Server_name}-KeyPassword.txt", "w") as f:
+        f.write(password)
+        f.write("\n")
+        f.close()
 
-# Wrtie cert to file
-with open("ServerCert.pem", "wb") as f:
-    f.write(server_cert.public_bytes(encoding=serialization.Encoding.PEM))
-    f.close()
+    # Generate certificate for the database
+        subject = x509.Name([
+        x509.NameAttribute(NameOID.COMMON_NAME, Server_name),
+    ])
+        db_cert = x509.CertificateBuilder().subject_name(
+        subject
+    ).issuer_name(
+        ca_name
+    ).public_key(
+        PubKey
+    ).serial_number(
+        x509.random_serial_number()
+    ).not_valid_before(
+        datetime.datetime.utcnow()
+    ).not_valid_after(
+        datetime.datetime.utcnow().replace(year=datetime.datetime.utcnow().year + 1)
+    ).sign(CAkey, hashes.SHA256())
 
+    # Wrtie cert to file
+    with open(f"out/{Server_name}-Cert.pem", "wb") as f:
+        f.write(db_cert.public_bytes(encoding=serialization.Encoding.PEM))
+        f.close()
 
+gen_key_cert_signed(ca_name,CAkey, "ca")
+gen_key_cert_signed(ca_name,CAkey, "db")
+gen_key_cert_signed(ca_name,CAkey, "www")
+gen_key_cert_signed(ca_name,CAkey, "fw")
+gen_key_cert_signed(ca_name,CAkey, "bkp")
 
-
-# Generate private and public key of the database
-DBkey = ec.generate_private_key(ec.SECP384R1())
-DbPubKey = DBkey.public_key()
-with open("DBkey.pem", "wb") as f:
-    f.write(DBkey.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.TraditionalOpenSSL,
-        encryption_algorithm=serialization.BestAvailableEncryption(b"password"),
-    ))
-    f.close()
-
-# Generate certificate for the database
-subject = x509.Name([
-    x509.NameAttribute(NameOID.COMMON_NAME, u"Database"),
-])
-db_cert = x509.CertificateBuilder().subject_name(
-    subject
-).issuer_name(
-    ca_name
-).public_key(
-    ServerPubKey
-).serial_number(
-    x509.random_serial_number()
-).not_valid_before(
-    datetime.datetime.utcnow()
-).not_valid_after(
-    datetime.datetime.utcnow().replace(year=datetime.datetime.utcnow().year + 1)
-).sign(CAkey, hashes.SHA256())
-
-# Wrtie cert to file
-with open("DBcert.pem", "wb") as f:
-    f.write(db_cert.public_bytes(encoding=serialization.Encoding.PEM))
-    f.close()
