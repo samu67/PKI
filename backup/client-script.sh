dd2d208ceb824b34469a8e8113fc223c4bebf496
ccharollais <ccharollais@ethz.ch>
2021-11-30 17:49:57 +0100

    backup mechanism

--- backup/client-script.sh
+++ backup/client-script.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
 
-RECIPIENT_PK='/Users/usr/Documents/ETH/AppliedSecLab/Project/git/credentials/ssh/backup/enc.pub'
 
-ID_DIR = ''
+HNAME='ca'
+
 
 FOLDERS_TO_BKP=(\
 '/Users/usr/Documents/ETH/AppliedSecLab/Project/git/backup/test2' \
 '/Users/usr/Documents/ETH/AppliedSecLab/Project/git/backup/test');
 
-TMP_FOLDER_DIR="/Users/usr/Documents/ETH/AppliedSecLab/Project/git/backup";
+
+
+TMP_FOLDER_DIR="/root/backup/";
+ENC_KEY='/root/backup/enc.pub'
+ID_DIR="/root/.ssh"
 TMP_FOLDER=$(mktemp -d ${TMP_FOLDER_DIR}/_XXXXXXXXXX);
-# trap 'rm -rf $TMP_FOLDER' EXIT;
+
+trap 'rm -rf $TMP_FOLDER' EXIT;
 
 # Compression and encryption
 for FOLDER in "${FOLDERS_TO_BKP[@]}"; do
-    TAR_FN=$(echo "${TMP_FOLDER}")/$(echo "${FOLDER}".tar.gz | sed s!\/!! | sed s!\/!\-!g);
+    TAR_FN=$(echo "${TMP_FOLDER}")/$(date "+%Y-%m-%d_%H:%M_")$(echo "${FOLDER}".tar.gz | sed s!\/!! | sed s!\/!\-!g);
     tar -czf "${TAR_FN}" "${FOLDER}"
-    age --armor -R "${RECIPIENT_PK}" -o "${TAR_FN}".age "${TAR_FN}"
+    age --armor -R "${ENC_KEY}" -o "${TAR_FN}".age "${TAR_FN}"
     rm "${TAR_FN}"
 done
 
-# # transmission
-# for ENC_FILE in "${TMP_FOLDER}"/*.age; do
-#     scp -i "${ID_DIR}"/bkp-$(hostname)-usr "${ENC_FILE}" bkp-$(hostname)-usr@bkp.imovies.com:/
-# done
+# creating instructions for sftp
+TMP_BATCHFILE="${TMP_FOLDER}"/batch.batch
+touch "${TMP_BATCHFILE}";
+echo "put *.age backups/" > "${TMP_BATCHFILE}";
 
-# # decryption
-# for ENC_FILE in "${TMP_FOLDER}"/*.age; do
-#     DEC_FILE=$(basename "${ENC_FILE}" .tar.gz.age).tar.gz2
-#     age --decrypt -i /Users/usr/Documents/ETH/AppliedSecLab/Project/git/credentials/ssh/bkp/enc -o "${TMP_FOLDER}/${DEC_FILE}" "${ENC_FILE}";
-# done
+# transfer files
+sftp -i "${ID_DIR}"/bkp-"${HNAME}" -b "${TMP_BATCHFILE}" bkp-"${HNAME}"@bkp.imovies.com;
 
